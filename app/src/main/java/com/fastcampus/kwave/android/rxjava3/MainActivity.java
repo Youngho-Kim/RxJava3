ee86c9b8facdc8d575252155402c2c7cd78858e7
kwave <kwvae55@gmail.com>
2017-07-20 11:31:57 +0900

    RxJava Subject

--- app/src/main/java/com/fastcampus/kwave/android/rxjava3/MainActivity.java
+++ app/src/main/java/com/fastcampus/kwave/android/rxjava3/MainActivity.java
@@ -10,7 +10,10 @@ import android.widget.Button;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.AsyncSubject;
+import io.reactivex.subjects.BehaviorSubject;
 import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.ReplaySubject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,51 +40,52 @@ public class MainActivity extends AppCompatActivity {
 
     PublishSubject<String> publishSubject = PublishSubject.create();
     public void doPublish(View view) {
-//        // 발행
-//        publishSubject.onNext("A");
-//        publishSubject.onNext("B");
-//        publishSubject.onNext("C");
-//
-//        // 구독
-//        // publishSubject는 구독 이후의 항목들에 대해서만 출력된다.
-//
-//        //        publishSubject.subscribe(new Observer<String>() {
-//        //                                     @Override
-//        //                                     public void onSubscribe(Disposable d) {
-//        //
-//        //                                     }
-//        //
-//        //                                     @Override
-//        //                                     public void onNext(String value) {
-//        //
-//        //                                     }
-//        //
-//        //                                     @Override
-//        //                                     public void onError(Throwable e) {
-//        //
-//        //                                     }
-//        //
-//        //                                     @Override
-//        //                                     public void onComplete() {
-//        //
-//        //                                     }
-//        //                                 }
-//        //        );
-//        //    ==>
-//                publishSubject.subscribe(
-//                item -> Log.i("publish", "item"+item)
-//        );
-//        publishSubject.onNext("D");
-//        publishSubject.onNext("E");
-//        publishSubject.onNext("F");
-//        publishSubject.onNext("G");
+/*
+        // 발행
+        publishSubject.onNext("A");
+        publishSubject.onNext("B");
+        publishSubject.onNext("C");
 
+        // 구독
+        // publishSubject는 구독 이후의 항목들에 대해서만 출력된다.
+
+        //        publishSubject.subscribe(new Observer<String>() {
+        //                                     @Override
+        //                                     public void onSubscribe(Disposable d) {
+        //
+        //                                     }
+        //
+        //                                     @Override
+        //                                     public void onNext(String value) {
+        //
+        //                                     }
+        //
+        //                                     @Override
+        //                                     public void onError(Throwable e) {
+        //
+        //                                     }
+        //
+        //                                     @Override
+        //                                     public void onComplete() {
+        //
+        //                                     }
+        //                                 }
+        //        );
+        //    ==>
+                publishSubject.subscribe(
+                item -> Log.i("publish", "item"+item)
+        );
+        publishSubject.onNext("D");
+        publishSubject.onNext("E");
+        publishSubject.onNext("F");
+        publishSubject.onNext("G");
+*/
         new Thread(){
             @Override
             public void run() {
                 // 발행
-                for (int i = 0; i < 100; i++) {
-                    publishSubject.onNext("A"+i);
+                for (int i = 0; i < 10; i++) {
+                    behaviorSubject.onNext("A"+i);           // 옵저버블이 발행
                     Log.i("Publish","A"+i);
                     try {
                         Thread.sleep(1000);
@@ -94,27 +98,94 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     public void getPublish(View view) {
-        publishSubject.observeOn(AndroidSchedulers.mainThread());
-        publishSubject.subscribe(
+        publishSubject.observeOn(AndroidSchedulers.mainThread());   // 옵저버가 메인쓰레드를 구독하기 시작
+        publishSubject.subscribe(                                   // 옵저버가 내용을 출력
                 item -> Log.i("publish", "item"+item)
         );
     }
 
 
-    public void doStart(View view) {
-    }
-
-    public void doAsync(View view) {
-    }
-
-    public void doReplay(View view) {
-    }
-
+    BehaviorSubject<String> behaviorSubject = BehaviorSubject.create();
     public void doBehavior(View view) {
+        new Thread(){
+            @Override
+            public void run() {
+                // 발행
+                for (int i = 0; i < 10; i++) {
+                    publishSubject.onNext("B"+i);           // 옵저버블이 발행
+                    Log.i("behavior","B"+i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
     }
 
+    public void getBehavior(View view) {
+        behaviorSubject.subscribe(                                   // 옵저버가 내용을 출력
+                item -> Log.i("behavior", "item"+item)
+        );
+    }
+
+    ReplaySubject<String> replaySubject = ReplaySubject.create();
+    public void doReplay(View view) {
+        new Thread(){
+            @Override
+            public void run() {
+                // 발행
+                for (int i = 0; i < 10; i++) {
+                    replaySubject.onNext("C"+i);           // 옵저버블이 발행
+                    Log.i("replay","C"+i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
+
+    public void getReplay(View view) {
+        replaySubject.subscribe(                                   // 옵저버가 내용을 출력
+                item -> Log.i("replay", "item"+item)
+        );
+    }
+
+
+    AsyncSubject<String> asyncSubject = AsyncSubject.create();
+    public void doAsync(View view) {
+        new Thread(){
+            @Override
+            public void run() {
+                // 발행
+                for (int i = 0; i < 10; i++) {
+                    asyncSubject.onNext("C"+i);           // 옵저버블이 발행
+                    Log.i("Async","C"+i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
+
+    public void getAsync(View view) {
+        asyncSubject.subscribe(                                   // 옵저버가 내용을 출력
+                item -> Log.i("Async", "item"+item)
+        );
+    }
+
+
+    public void doStart(View view) {
+
+    }
 
 
 }
